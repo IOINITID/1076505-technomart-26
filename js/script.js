734f3e48d32817908f73ebfd36acb9b48792d2f2
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-10-14 15:59:05 +0300

    Added switching slider and block with information

---
+++ js/script.js
@@ -0,0 +1,113 @@
+// modals
+var mapButton = document.querySelector('.contacts__map-button');
+var map = document.querySelector('.map-popup');
+var closeModalMap = document.querySelector('.close-button-map');
+var writeUsButton = document.querySelector('.contacts__link');
+var writeUsPopup = document.querySelector('.write-us');
+var closeModalInfo = document.querySelector('.close-button-info');
+var cartButton = document.querySelector('.item__buy');
+var cart = document.querySelector('.cart-popup');
+var closeModalCart = document.querySelector('.close-button-cart');
+
+if (mapButton !== null) {
+  mapButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    map.classList.remove('hide');
+  });
+}
+if (closeModalMap !== null) {
+  closeModalMap.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    map.classList.add('hide');
+  });
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      map.classList.add('hide');
+    }
+  });
+}
+if (writeUsButton !== null) {
+  writeUsButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.remove('hide');
+  });
+}
+if (closeModalInfo !== null) {
+  closeModalInfo.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    writeUsPopup.classList.add('hide');
+  });
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      writeUsPopup.classList.add('hide');
+    }
+  });
+}
+if (cartButton !== null) {
+  cartButton.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    cart.classList.remove('hide');
+  });
+}
+if (closeModalCart !== null) {
+  closeModalCart.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    cart.classList.add('hide');
+  });
+  window.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === 27) {
+      cart.classList.add('hide');
+    }
+  });
+}
+// tabs
+var deliverLink = document.querySelector('.features__link-deliver');
+var guaranteeLink = document.querySelector('.features__link-guarantee');
+var creditLink = document.querySelector('.features__link-credit');
+var deliverBlock = document.querySelector('.features__deliver');
+var guaranteeBlock = document.querySelector('.features__guarantee');
+var creditBlock = document.querySelector('.features__credit');
+
+if (deliverLink !== null) {
+  deliverLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    deliverBlock.classList.remove('hide');
+    guaranteeBlock.classList.add('hide');
+    creditBlock.classList.add('hide');
+    deliverLink.classList.add('features__link--active');
+    guaranteeLink.classList.remove('features__link--active');
+    creditLink.classList.remove('features__link--active');
+  });
+}
+if (guaranteeLink !== null) {
+  guaranteeLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    deliverBlock.classList.add('hide');
+    guaranteeBlock.classList.remove('hide');
+    creditBlock.classList.add('hide');
+    deliverLink.classList.remove('features__link--active');
+    guaranteeLink.classList.add('features__link--active');
+    creditLink.classList.remove('features__link--active');
+  });
+}
+if (creditLink !== null) {
+  creditLink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    deliverBlock.classList.add('hide');
+    guaranteeBlock.classList.add('hide');
+    creditBlock.classList.remove('hide');
+    deliverLink.classList.remove('features__link--active');
+    guaranteeLink.classList.remove('features__link--active');
+    creditLink.classList.add('features__link--active');
+  });
+}
+
+document.querySelector('.slider__next').addEventListener('click', function (evt) {
+  evt.preventDefault();
+  document.querySelector('.slider__two').classList.add('slider-next');
+});
+
+document.querySelector('.slider__prev').addEventListener('click', function (evt) {
+  evt.preventDefault();
+  document.querySelector('.slider__two').classList.remove('slider-next');
+});
